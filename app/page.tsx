11540874a2e20bdf62c3511cf8d60a2e2e0f8f1d
Pascal Zemp <zemp57@gmail.com>
2026-07-05 21:37:00 +0200

    Seite kompakter gemacht

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 import About from "./components/About";
 import Calculator from "./components/Calculator";
 import Contact from "./components/Contact";
-import Discover from "./components/Discover";
-import Intro from "./components/Intro";
 import GoldKnowledge from "./components/GoldKnowledge";
-import ConceptFlow from "./components/ConceptFlow";
 import FAQ from "./components/FAQ";
 import Hero from "./components/Hero";
-import HowItWorks from "./components/HowItWorks";
 import Navbar from "./components/Navbar";
-import Principle from "./components/Principle";
 import Stats from "./components/Stats";
-import Why from "./components/Why";
+import Intro from "./components/Intro";
 
 export default function Home() {
   return (
@@ -19,11 +14,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Intro />
-      <Why />
       <GoldKnowledge />
-      <Principle />
-      <ConceptFlow />
-      <Discover />
       <Calculator />
       <Stats />
       <FAQ />
